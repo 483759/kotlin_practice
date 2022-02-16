4590f6c078701cf61062fbfab0e3b75448003df5
binary.yun <binary.yun@kakaocorp.com>
2022-02-16 11:27:53 +0900

    [spring-boot-kotlin]style: 스타일 설정 체크

--- blog/src/main/kotlin/com/example/blog/BlogProperties.kt
+++ blog/src/main/kotlin/com/example/blog/BlogProperties.kt
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.ConstructorBinding
  * */
 @ConstructorBinding
 @ConfigurationProperties("blog")
-data class BlogProperties(var title: String, val banner: Banner) {
+data class BlogProperties        (var title: String, val banner: Banner) {
+
     data class Banner(val title: String? = null, val content: String)
 }
